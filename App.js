2387deebea0b138a666182661a18a9661a17b49c
Steven <Steven.madi@edu.devinci.fr>
2023-02-24 23:23:21 +0100

    :tada: init react native with expo

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground,TextInput } from 'react-native';
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { ActivityIndicator } from 'react-native-web';
+import {KEY} from '@env';
 
-export default function App() {
+const  App = () => {
+  const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+const api = { 
+  key: KEY,
+  url: 'https://api.openweathermap.org/data/3.0/'
+}
+  
+
+  const fetchData = useCallback(() => {
+    setLoading(true);
+    setInput('');
+    axios({
+      method:'GET',
+      url:`https://api.openweathermap.org/data/3.0/weather?q=${input}$units=metric$appid=${api.key}`,
+    })
+    .then(res=>{
+      
+    })
+  }, [api.key , input]);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/img/bg.jpg')} resizeMode='cover' style={styles.image}>
+        <View>
+          <TextInput placeholder='Entrer le nom de la ville' 
+            style={styles.textInput} 
+            onChangeText={text => setInput(text)}
+            value={input}
+            onSubmitEditing={fetchData}
+          />
+        </View>
+        {loading && (
+          <View>
+            <ActivityIndicator></ActivityIndicator>
+          </View>
+        )};
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  image:{
+    flex:1,
+    flexDirection:'column',
+  },
+  textInput:{
+    borderBottomWidth:3,
+    backgroundColor:'#fff',
+  }
 });
+
+
+
+export default App;
